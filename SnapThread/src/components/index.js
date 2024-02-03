5b861d637a2217cd861db98170a4f9fe8040113e
Azaad Mottan <azaadmottan2022@gmail.com>
2024-02-03 18:10:04 +0530

    Update directory name 'project' to 'SnapThread'.

---
+++ SnapThread/src/components/index.js
@@ -0,0 +1,16 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Loading from "./Loading/Loading";
+import Logo from "./Logo/Logo";
+import SignIn from "./SignIn/SignIn";
+import SignUp from "./SignUp/SignUp";
+
+
+export {
+    Header,
+    Footer,
+    Loading,
+    Logo,
+    SignIn,
+    SignUp,
+}
