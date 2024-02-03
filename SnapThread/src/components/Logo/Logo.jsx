ab4a38e307756e1b509ad1b7019eafdc8bac9cd5
Azaad Mottan <azaadmottan2022@gmail.com>
2024-02-03 18:48:00 +0530

    Create logo component.

--- SnapThread/src/components/Logo/Logo.jsx
+++ SnapThread/src/components/Logo/Logo.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import LogoIcon from "../../assets/logo-512x512.png";
 
-function Logo() {
+function Logo(
+    imageWidth = "w-20",
+    textSize = "text-xl"
+) {
+
     return (
-        <div>Logo</div>
-    )
+    <>
+        <div className='flex items-center gap-4'>
+            <img 
+                src={LogoIcon} alt="SnapThread" 
+                className={`${imageWidth}`}
+            />
+            <span className={`${textSize}`}>SnapThread</span>
+        </div>
+    </>
+    );
 }
 
 export default Logo;
