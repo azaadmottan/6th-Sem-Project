8e6d917847caa29e4e2da02df462fdfa904744fa
Azaad Mottan <azaadmottan2022@gmail.com>
2024-02-04 18:23:39 +0530

    Config tailwind file for theme modes and setup routes for pages and also setup Provider & RouterProvider.

--- SnapThread/src/App.jsx
+++ SnapThread/src/App.jsx
@@ -1,12 +1,50 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Footer, Header, Loading } from "./components/index.js";
+import { Outlet } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import authService from "./services/authService.js";
+import { login, logout } from "./slicer/authSlicer.js";
+
 
 function App() {
 
-   return (
-      <>
-      <h2 className="text-4xl text-white bg-zinc-700">SnapThread</h2>
-      </>
-   );
+   const [loading, setLoading] = useState(true);
+
+   const dispatch = useDispatch();
+
+   useEffect(() => {
+
+      authService.getCurrentUser()
+      .then((userData) => {
+   
+         if(userData) {
+   
+            dispatch(login());
+         }
+         else {
+   
+            dispatch(logout());
+         }
+      })
+      .finally(() => setLoading(false));
+
+   }, []);
+   
+
+   return (!loading) ? (<>
+
+      <Header />
+
+         <main className="dark:bg-[#0b0143]">
+
+            <Outlet />
+
+         </main>
+
+      <Footer />
+
+   </>) : (<Loading />);
+
 }
 
 export default App;
--- SnapThread/src/main.jsx
+++ SnapThread/src/main.jsx
@@ -2,12 +2,131 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { Provider } from "react-redux";
+import store from "./store/store.js";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { ProtectRoutes, SignIn, SignUp } from "./components/index.js";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Blog from "./pages/Blog.jsx";
+import Business from "./pages/Business.jsx";
+import Explore from "./pages/Explore.jsx";
+import Watch from "./pages/Watch.jsx";
+import Today from "./pages/Today.jsx";
+import AddNewPost from "./pages/AddNewPost.jsx";
+import Post from "./pages/Post.jsx";
+import AllPosts from "./pages/AllPosts.jsx";
+import EditPost from "./pages/EditPost.jsx";
+import HandleRoute from "./pages/HandleRoute.jsx";
+
+
+const router = createBrowserRouter([
+
+   {
+      path: "/",
+      element: <App />,
+      children: [
+         {
+            path: "/",
+            element: <Home />,
+            
+         },
+         {
+            path: "/today",
+            element: <Today />,
+            
+         },
+         {
+            path: "/watch",
+            element: <Watch />,
+            
+         },
+         {
+            path: "/explore",
+            element: <Explore />,
+            
+         },
+         {
+            path: "/about",
+            element: <About />,
+            
+         },
+         {
+            path: "/business",
+            element: <Business />,
+            
+         },
+         {
+            path: "/blog",
+            element: <Blog />,
+            
+         },
+         {
+            path: "/signUp",
+            element: (
+               <ProtectRoutes authentication={false} >
+                  <SignUp />
+               </ProtectRoutes>
+            )
+         },
+         {
+            path: "/signIn",
+            element: (
+               <ProtectRoutes authentication={false}>
+                  <SignIn />
+               </ProtectRoutes>
+            )
+         },
+         {
+            path: "/all-posts",
+            element: (
+               <ProtectRoutes authentication>
+                  <AllPosts />
+               </ProtectRoutes>
+            )
+         }, 
+         {
+            path: "/add-new-post",
+            element: (
+               <ProtectRoutes authentication>
+                  <AddNewPost />
+               </ProtectRoutes>
+            )
+         }, 
+         {
+            path: "/edit-post/:slug",
+            element: (
+               <ProtectRoutes authentication>
+                  <EditPost />
+               </ProtectRoutes>
+            )
+         }, 
+         {
+            path: "/post/:slug",
+            element: (
+               <ProtectRoutes authentication>
+                  <Post />
+               </ProtectRoutes>
+            )
+         },
+         {
+            path: "*",
+            element: <HandleRoute />,
+         }
+      ]
+   }
+]);
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 
    <React.StrictMode>
 
-      <App />
+      <Provider store={store}>
+
+         <RouterProvider router={router} />
+
+      </Provider>
       
    </React.StrictMode>
 );
