a981e7815aca82cf4aa0efca97a2c30e1b13583e
Azaad Mottan <azaadmottan2022@gmail.com>
2024-02-04 18:20:13 +0530

    Add components in index.js file.

--- SnapThread/src/components/index.js
+++ SnapThread/src/components/index.js
@@ -4,7 +4,12 @@ import Loading from "./Loading/Loading";
 import Logo from "./Logo/Logo";
 import SignIn from "./SignIn/SignIn";
 import SignUp from "./SignUp/SignUp";
-
+import PostForm from "./PostForm/PostForm";
+import PostCard from "./PostCard/PostCard";
+import ProtectRoutes from "./ProtectRoutes";
+import LogoutBtn from "./Header/LogoutBtn";
+import Processing from "./Processing/Processing";
+import ThemeBtn from "./ThemeBtn/ThemeBtn";
 
 export {
     Header,
@@ -13,4 +18,10 @@ export {
     Logo,
     SignIn,
     SignUp,
+    LogoutBtn,
+    PostForm,
+    PostCard,
+    ProtectRoutes,
+    Processing,
+    ThemeBtn
 }
