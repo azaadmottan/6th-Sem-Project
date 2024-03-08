2e5b05de2ea5a824b0678a8276f7e670078ae868
Azaad Mottan <azaadmottan2022@gmail.com>
2024-03-08 21:17:21 +0530

    Add conditional rendering of blog posts based on user authentication status.

--- SnapThread/src/pages/Blog.jsx
+++ SnapThread/src/pages/Blog.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import confService from '../services/confService.js';
 import { MiniPostCard, Logo } from "../components";
+import { useSelector } from "react-redux";
 
 function Blog() {
 
@@ -28,41 +29,55 @@ function Blog() {
             <h2>Mini Blogs</h2>
         </div>
 
-        <div className='dark:bg-[#0d043c] grid gap-4 md:grid-cols-3 lg:grid-cols-4 grid-cols-1 p-4 mt-4 rounded-xl'>
+        {   
+            (!(useSelector((state) => state.auth.status))) ? (
+                
+                    
+                    <div className='dark:bg-[#0d043c] p-4 mt-4 rounded-xl'>
+                        <div className="p-3">
+                            <h2 className="text-center text-xl text-white">Sign in now  to see the blog posts!</h2>
+                        </div>
+                    </div>
+            ) : (
 
-            {(!loading) ? (
+                <div className='dark:bg-[#0d043c] grid gap-4 md:grid-cols-3 lg:grid-cols-4 grid-cols-1 p-4 mt-4 rounded-xl'>
 
-                    (allPosts.length > 0) ? (
+                    {(!loading) ? (
 
-                        allPosts.map((post) => (
+                            (allPosts.length > 0) ? (
 
-                            <div key={post.$id} className='md:w-3/3 lg:w-4/4 md:p-2'>
+                                allPosts.map((post) => (
 
-                                <MiniPostCard {...post} />
+                                    <div key={post.$id} className='md:w-3/3 lg:w-4/4 md:p-2'>
+
+                                        <MiniPostCard {...post} />
+
+                                    </div>
+                                ))
+                            ) : (
+                                <div className='w-full h-16 bg-white rounded-xl flex items-center justify-center'>
+                                    <h2 className='text-xl font-normal text-red-600 animate-bounce dark:text-white'>No Post has been Found.</h2>
+                                </div>
+                            )
+                            
+                        ) : (
+                            
+                            <div className='flex flex-col md:w-[75vw] m-auto  items-center justify-center w-full'>
+
+                                <div className='w-20 h-20 border-2 border-[#df2121] rounded-full animate-spin border-t-transparent'></div>
+
+                                <div className='flex items-center mt-10 bg-zinc-50 rounded-xl px-2 py-1 md:px-20 md:py-2'>
+                                    <Logo />
+                                </div>
 
                             </div>
-                        ))
-                    ) : (
-                        <div className='w-full h-16 bg-white rounded-xl flex items-center justify-center'>
-                            <h2 className='text-xl font-normal text-red-600 animate-bounce dark:text-white'>No Post has been Found.</h2>
-                        </div>
-                    )
-                    
-                ) : (
-                    
-                    <div className='flex flex-col md:w-[75vw] m-auto  items-center justify-center w-full'>
+                        )
+                    }
 
-                        <div className='w-20 h-20 border-2 border-[#df2121] rounded-full animate-spin border-t-transparent'></div>
+                </div>
+            )
 
-                        <div className='flex items-center mt-10 bg-zinc-50 rounded-xl px-2 py-1 md:px-20 md:py-2'>
-                            <Logo />
-                        </div>
-
-                    </div>
-                )
-            }
-
-        </div>
+        }
 
     </div>
     </>
